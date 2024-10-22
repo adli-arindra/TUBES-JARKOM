1c1f1f109a063f5c47898974a060d68447c51b87
Adli Arindra <adliarindra27@gmail.com>
2024-10-22 19:47:41 +0700

    init

---
+++ server.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+
+HEADER = 64
+PORT = 5050
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDRESS = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MSG = "!DISC"
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(ADDRESS)
+
+def handle_client(conn, addr):
+    print(f"NEW CONNECTION: {addr}\n")
+
+    connected = True
+    while connected:
+        msg_length = int(conn.recv(HEADER).decode(FORMAT))
+        msg = conn.recv(msg_length).decode(FORMAT)
+        if msg == DISCONNECT_MSG:
+            connected = False
+    
+        print(f"[{addr}] : {msg}")
+
+    conn.close()
+
+
+def start():
+    server.listen()
+    print(f"SERVER IS LISTENING ON {SERVER}")
+    while True:
+        conn, address = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, address))
+        thread.start()
+        print(f"ACTIVE CONNECTIONS : {threading.activeCount() - 1}")
+
+if __name__ == "__main__":
+    print("SERVER IS STARTING...")
+    start()
