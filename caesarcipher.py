66d11102033d52d58823bad29266b7473a0b63b5
Japrulll <18223067@std.stei.itb.ac.id>
2024-11-01 18:55:44 +0700

    Caesar Update

--- caesarcipher.py
+++ caesarcipher.py
@@ -1,21 +1,27 @@
 def cypher_enc(text):
     encrypt = ""
     for i in range (len(text)):
-        lower_case = text[i].lower()
-        shift = 1
-        encrypt += chr(((ord(lower_case) + shift - 97) % 26) + 97)
+        if ord(text[i]) == 32:
+            encrypt += "%"
+        else: 
+            lower_case = text[i].lower()
+            shift = 1
+            encrypt += chr(((ord(lower_case) + shift - 97) % 26) + 97)
     return encrypt
 
 def cypher_dec(text):
     decrypt = ""
     for i in range (len(text)):
-        lower_case = text[i].lower()
-        shift = 25
-        decrypt += chr(((ord(lower_case) + shift - 97) % 26) + 97)
+        if ord(text[i]) == 37:
+            decrypt += " "
+        else: 
+            lower_case = text[i].lower()
+            shift = 25
+            decrypt += chr(((ord(lower_case) + shift - 97) % 26) + 97)
     return decrypt
 
 
-a = "fufUfAFa"
+a = "atha adam"
 new = ['0' for _ in range (len(a))]
 enc = cypher_enc(a)
 print(enc)
